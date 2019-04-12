3e451b8999a5d74cf540c60c88afd7bb1cc5f715
Marcelo Bertizini <marcelo.bertizini@etec.sp.gov.br>
2019-04-11 21:23:49 -0300

    vinculação do banco de dados com a pagina web

--- src/com/agenda/dao/PessoaDAO.java
+++ src/com/agenda/dao/PessoaDAO.java
@@ -1,11 +1,37 @@
 package com.agenda.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.agenda.modelo.Pessoa;
+import com.agenda.util.ConnectionFactory;
+import com.mysql.jdbc.Connection;
 
 public class PessoaDAO {
 	
-	public void adiciona(Pessoa pessoa) {
+	private Connection connection;
+	
+	public void recebe(Pessoa pessoa) {
 		String SQL = "Insert into pessoas(nome, email, endereco, telefone) values(?,?,?,?)";
+	
+	try {
+		this.connection = new ConnectionFactory().getConnection();
+		PreparedStatement stmt = this.connection.prepareStatement(SQL);
+		
+		stmt.setString(1, pessoa.getNome());
+		stmt.setString(2, pessoa.getEmail());
+		stmt.setString(3, pessoa.getPhone());
+		stmt.setString(4, pessoa.getEnd());
+	
+		stmt.execute();
+		stmt.close();
+		
+	}catch (SQLException e) {
+		throw new RuntimeException(e);
+		
+	}
+	
+	
 	}
 
 }
